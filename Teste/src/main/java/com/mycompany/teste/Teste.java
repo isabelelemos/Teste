ee2a110356e4b8ff2e84a1d04c45d1f33f2e6fdd
isats <isats@DESKTOP-GEEBKUA>
2023-06-04 19:42:04 -0300

    Commit de teste

---
+++ Teste/src/main/java/com/mycompany/teste/Teste.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teste;
+
+/**
+ *
+ * @author isats
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.println("Testando commit");
+    }
+}
